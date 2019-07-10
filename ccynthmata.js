1f2bf4c23e8f5a21be94df0ba4709956a6ae9784
oscillatorsink <31288652+oscillatorsink@users.noreply.github.com>
2019-07-10 23:01:06 +0100

    Working on serializaing the patch

--- ccynthmata.js
+++ ccynthmata.js
@@ -92,6 +92,7 @@ function tryAttachParameterChangeHandler(ccControl){
 
 function setupParameterControls() {
     for(let ccControl of getCcElements()){
+        // TODO: validate the channel and cc numbers here, and remove the class from elements with invalid values
         if(!tryAttachParameterChangeHandler(ccControl)){
             console.log(`Couldn't attach parameter watcher to ${ccClass} control:`)
             console.log(ccControl);
@@ -108,23 +109,17 @@ function* getCcElements(){
 }
 
 function getCcElementDetails(ele){
+    let ccLsb = "cclsb" in ele.dataset ? parseInt(ele.dataset.cclsb) : undefined;
+    let ccMsb = "ccmsb" in ele.dataset ? parseInt(ele.dataset.ccmsb) : undefined;
+    let overrideMidiChannel = ele.dataset.midichannel ? parseInt(ele.dataset.midichannel) - 1 : undefined;
+
     if (ele.classList.contains(MIDI_CC_PARAM_CLASS)) {
-        //let messageChannel = ele.dataset.midichannel ? parseInt(ele.dataset.midichannel) - 1 : selectedMidiChannel;
-        let overrideMidiChannel = ele.dataset.midichannel ? parseInt(ele.dataset.midichannel) - 1 : undefined;
-        let ccLsb = parseInt(ele.dataset.cclsb);
-        let ccMsb = parseInt(ele.dataset.ccmsb);
         let ccValue = parseInt(ele.value);
 
-        // TODO: Handle 14-bit parameters
-        
-        // send lsb/single-byte
         return {channel: overrideMidiChannel, ccLsbNumber: ccLsb, ccMsbNumber: ccMsb, value: ccValue};
     }else if (ele.classList.contains(MIDI_CC_TOTAL_CLASS)){
         // we need to total everything for this change's cc number and channel then send it all at once
         // currently, for simplicity, this type only supports single byte cc
-        let ccLsb = parseInt(ele.dataset.cclsb);
-        let overrideMidiChannel = ele.dataset.midichannel ? parseInt(ele.dataset.midichannel) - 1 : undefined;
-        
         let toTotal = [...document.getElementsByClassName(MIDI_CC_TOTAL_CLASS)]
             .filter(
                 x => parseInt(x.dataset.cclsb) === ccLsb && ((overrideMidiChannel === null && !x.dataset.midichannel) || overrideMidiChannel === parseInt(x.dataset.midichannel) - 1));
@@ -163,7 +158,101 @@ function _sendCcMessage(options){
     selectedMidiPort.send(paramChangeMessage);
 }
 
+function collectPatch(){
+    // collects all the parameters into an object; up to 17 keys: one for each channel and one for the user specified channel
 
+    let patchDetails = {};
+    for(let ccElement of getCcElements()){
+        let ccDetails = getCcElementDetails(ccElement);
+        let channel = ccDetails.channel === undefined ? 0x7f : ccDetails.channel;
+        if(!(channel in patchDetails)){
+            patchDetails[channel] = {}
+        }
+        // if we have multiple controls colliding on channel/cc the behaviour will be to use the one latest in the DOM - this is an arbitrary decision because it's
+        // less code to do it that way
+        console.log(ccDetails);
+        if(ccDetails.ccLsbNumber !== undefined){
+            
+            // this should always be here, but I'm being slightly more cautious because messing up the serialization will make me sad.
+            patchDetails[channel][ccDetails.ccLsbNumber] = ccDetails.value & 0x7f;
+        }
+        if(ccDetails.ccMsbNumber !== undefined){
+            patchDetails[channel][ccDetails.ccMsbNumber] = (ccDetails.value >> 7) & 0x7f;
+        }
+        
+    }
+    return patchDetails;
+}
+
+function serializePatch(){
+    // Serialized format goes like this:
+    // Header
+    // [Channel CCs]
+    //
+    // Header:
+    // ------           ------          -----------
+    // Offset           Length          Description
+    // ------           ------          -----------
+    // 0                1               Serialization version (must be 0x00)
+
+    // Then follows a CC structure for each channel used (will often just be the user-defined channel)
+    // CC Structure:
+    // ------           ------          -----------    
+    // Offset           Length          Description
+    // ------           ------          -----------
+    // 0                1               Channel number (0x00 = 1 - 0x0f = 16, 0x7f = user-defined)
+    // 1                1               <parameter_count> Parameter count - 1 (ie 0 means 1 parameter present - if no parameters, channel isn't serialized at all) (max 0x7f)
+    //          then <parameter_count> of:
+    // 2 + (n * 2)      1               CC number
+    // 3 + (n * 2)      1               CC value
+    //
+    // The data is then packed as per packBytes()
+
+    let patch = collectPatch();
+
+    let serializedRaw = [
+        0x00,
+    ];
+
+    for(let channel in patch){
+        serializedRaw.push(channel & 0x7f);
+        let channelCcs = patch[channel]
+        let paramCount = Object.keys(channelCcs).length;
+        if(paramCount > 128){
+            throw "illegal number of CC parameters";
+        }
+        serializedRaw.push(paramCount - 1);
+        for(let ccNumber in channelCcs){
+            serializedRaw.push(ccNumber & 0x7f);
+            serializedRaw.push(channelCcs[ccNumber] & 0x7f);
+        }
+    }
+
+    return packBytes(serializedRaw);
+
+}
+
+function packBytes(unpackedBytes){
+    // This seems like overkill, but as we have to go to Base64 and incur a 4/3 size increase and 
+    //  there are likely limits on query-string lengh on some server configurations, this gets us
+    //  a little more space to work with with a 7/8 reduction.
+    // TODO: write up some details around likely limits on size/number of parameters 
+    // TODO: write unpackBytes() and test that in == out == in
+    if (unpackedBytes.length === 0){
+        return [];
+    }
+    let result = [unpackedBytes[0]];
+    for(let i = 1; i < unpackedBytes.length; i++){
+        let shift = i % 7;
+        if(shift !== 0){
+            result.push(unpackedBytes[i] >> shift)
+        }
+            
+        let mask = 0x7f >> (7 - shift);
+        result[result.length - 2] |= (unpackedBytes[i] & mask) << (8 - shift);
+    }
+    return result;
+}
 
 function storeOutputs(midiAccess) {
     midiOutPorts = new Array(...midiAccess.outputs.values());
